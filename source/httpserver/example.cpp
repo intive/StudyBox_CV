d5adbb4d814f1bfcbf1941f490b680e247d52377
Bartosz Rejman <brejman@wi.zut.edu.pl>
2016-03-16 02:05:53 +0100

    Move files to httpserver subdir.

--- source/httpserver/example.cpp
+++ source/httpserver/example.cpp
@@ -2,15 +2,12 @@
 #include "Socket.h"
 
 #include <chrono>
-#include <iostream>
 
-int main()
+void example()
 {
     Http::Server server("0.0.0.0", "80", [](const Http::Request&) 
     {
         std::this_thread::sleep_for(std::chrono::seconds(5)); // Symulacja przeci¹¿enia.
         return Http::Response(Http::Response::Status::Ok, "Ok", "text/plain");
     });
-
-    std::cout << server.run() << std::endl;
 }
