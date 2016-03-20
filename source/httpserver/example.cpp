c32424089c31c164cadbea3f1b1db9471c1e0899
Bartosz Rejman <brejman@wi.zut.edu.pl>
2016-03-20 20:58:19 +0100

    Add Server::run() to example.

--- source/httpserver/example.cpp
+++ source/httpserver/example.cpp
@@ -10,4 +10,6 @@ void example()
         std::this_thread::sleep_for(std::chrono::seconds(5)); // Symulacja przeci¹¿enia.
         return Http::Response(Http::Response::Status::Ok, "Ok", "text/plain");
     });
+
+    server.run();
 }
