a7f965cbc9341561fa718a9f6cf91180ecdfdd28
dwegrowski <dwegrowski@wp.pl>
2016-04-18 19:11:02 +0200

    Funkcja zamieniająca tekst na JSON

---
+++ source/text2flashcard/text2flashcard.cpp
@@ -0,0 +1,22 @@
+#include "text2flashcard.h"
+#include <vector>
+
+void textToJson(std::string& txt)
+{
+	std::vector<Markers> markers = findQA(txt);
+	Json array = Json::Array();
+	for (size_t i = 0; i < markers.size(); i++)
+	{
+		Json object = {
+			{ "begin" , markers[i].getStart() },
+			{ "end" , markers[i].getEnd() },
+			{ "type" , (int)markers[i].getType() },
+			{ "percentage_chance" , markers[i].getPercentageChance() }
+		};
+		array.push_back(object);
+	}
+	Json final = {
+		{ "result" , array }
+	};
+	final.serialize("fiszka.txt");
+}
---
+++ source/text2flashcard/text2flashcard.h
@@ -0,0 +1,8 @@
+#ifndef TEXT2FLASHCARD
+#define TEXT2FLASHCARD
+#include "../json/Json.hpp"
+#include "../textanalysis/text_analysis.h"
+
+void textToJson(std::string txt);
+
+#endif // TEXT2FLASHCARD
