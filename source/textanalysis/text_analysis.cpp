5c6d4b0ca187d2dc3f55565cd375a2be9ec50e9c
dwegrowski <dwegrowski@wp.pl>
2016-04-18 21:59:26 +0200

    Text analysis fixes

--- source/textanalysis/text_analysis.cpp
+++ source/textanalysis/text_analysis.cpp
@@ -1,31 +1,100 @@
 #include"text_analysis.h"
 #include<vector>
-
+#include<cctype>
+#include<algorithm>
 const std::vector<std::string> dictionary = {
-	"Czy","czy",
-	"Ile","ile",
-	"Gdzie","gdzie",
-	"Jak","jak",
-	"Dlaczego","dlaczego",
-	"Po co","po co",
-	"Czemu","czemu",
-	"Z kim","z kim",
-	"Z czym","z czym",
-	"Na czym","na czym",
-	"Kiedy","kiedy",
-	"Kim","kim",
-	"W ktÃ³rym","w ktÃ³rym","W ktorym","w ktorym",
-	"Kogo","kogo",
-	"Za co","za co",
-	"Co","co",
-	"U kogo","u kogo",
-	"Jakiego","jakiego",
-	"Dla jakiego","dla jakiego",
-	"Dla kogo","dla kogo",
-	"Komu","komu"
+	"czy",
+	"ile",
+	"gdzie",
+	"jak",
+	"dlaczego",
+	"po co",
+	"czemu",
+	"z kim",
+	"z czym",
+	"na czym",
+	"kiedy",
+	"kim",
+	"w którym","w ktorym",
+	"kogo",
+	"za co",
+	"co",
+	"u kogo",
+	"jakiego",
+	"dla jakiego",
+	"dla kogo",
+	"komu"
 };
-std::vector<Markers> findQA(const std::string& text)
+
+//Funkcja wracaj¹ca od punktu end do najbli¿szego znaku interpunkcyjnego
+
+size_t findPreviousPunctuationMark(const std::string& text, size_t end)
 {
+	size_t startingMarker = end - 1;
+	while (true)
+	{
+		if (text[startingMarker] == '.' || text[startingMarker] == '!' || text[startingMarker] == '?')
+		{
+			startingMarker++;
+			break;
+		}
+		if (startingMarker != 0)
+			startingMarker--;
+		else
+			break;
+	}
+	return startingMarker;
+}
+
+//Funkcja sprawdzajaca czy zdanie jest puste albo z jednym znakiem
+
+bool checkIfEmptySentence(std::vector<Markers>& markersVector, const std::string& text, size_t start, size_t end)
+{
+	if (end - start < 2)
+	{
+		Markers unclasified(start, end, TextType::unclasified, 80);
+		markersVector.push_back(unclasified);
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+//Funkcja sprawdzaj¹ca czy s¹ samog³oski w zdaniu
+
+bool checkIfNoVowels(std::vector<Markers>& markersVector, const std::string& text, size_t start, size_t end)
+{
+	auto found2 = text.find_first_of("eyuioa", start);
+	if (found2 == std::string::npos || found2 > end)
+	{
+		Markers unclasified(start, end, TextType::unclasified, 95);
+		markersVector.push_back(unclasified);
+		return true;
+	}
+	else
+		return false;
+}
+
+//Funkcja sprawdzaj¹ca spacje w zdaniu
+
+bool checkIfNoSpaces(std::vector<Markers>& markersVector, const std::string& text, size_t start, size_t end)
+{
+	auto found3 = text.find_first_of(" ", start + 1);
+	if (end - start > 15 && (found3 > end || found3 == std::string::npos))
+	{
+		Markers unclasified(start, end, TextType::unclasified, 70);
+		markersVector.push_back(unclasified);
+		return true;
+	}
+	else
+		return false;
+}
+
+std::vector<Markers> findQA(std::string text)
+{
+	std::transform(text.begin(), text.end(),text.begin(), std::tolower);
 	std::vector<Markers> markersVector;
 	if (text.empty())
 		return markersVector;
@@ -33,89 +102,76 @@ std::vector<Markers> findQA(const std::string& text)
 	for (std::size_t i = 0; i<text.size(); ++i)
 	{
 		//Jesli wykryto znak zapytania
-		if (text[i] == '?') 
+		if (text[i] == '?')
 		{
 			//Wykrywanie przypadku gdy zdanie z niewiadomych przyczyn zaczyna sie znakiem zapytania
+
 			if (i == 0)
 			{
 				continue;
 			}
-			std::size_t startingMarker = i - 1;
+			
+			//"Cofanie siê" dopoki nie znajdzie sie znaku interpunkcyjnego lub poczatku
 
-			//"Cofanie siÄ™" dopoki nie znajdzie sie znaku interpunkcyjnego lub poczatku
-			while (startingMarker != 0)
-			{
-				if (text[startingMarker] == '.' || text[startingMarker] == '!' || text[startingMarker] == '?')
-					break;
-				startingMarker--;
-			}
+			std::size_t startingMarker = findPreviousPunctuationMark(text, i);
 
 			//Sprawdzanie sytuacji typu ". ? " badz z jakims jednym znakiem pomiedzy
-			if (i - startingMarker + 1 < 2)
-			{
-				Markers unclasified(startingMarker, i, TextType::unclasified, 80);
-				markersVector.push_back(unclasified);
+
+			if (checkIfEmptySentence(markersVector, text, startingMarker, i))
 				continue;
-			}
-			else
+
+			auto found = std::string::npos;
+			std::size_t k = 0;
+			//Wyszukiwanie w zdaniu jednego ze slow kluczowych zdefiniowanych w dictionary, charakteryzujacych pytanie
+			while (k < dictionary.size())
 			{
-				auto found=std::string::npos;
-				std::size_t k = 0;
+				auto tmp = text.find(dictionary[k], startingMarker);
+				if (found != std::string::npos && tmp < found && tmp >= startingMarker)
+				{
+					found = tmp;
+				}
+				else if (found == std::string::npos)
+				{
+					found = text.find(dictionary[k], startingMarker);
+				}
+				k++;
+			}
+			//Jezeli znaleziono takie slowo kluczowe na poczatku zdania to na 100% jest to pytanie
+			if (found - startingMarker < 4 && found != std::string::npos && found < i)
+			{
+				Markers question(startingMarker, i, TextType::question, 100);
+				markersVector.push_back(question);
+			}
+			else if (found - startingMarker > 4 && found < i)  //w przeciwnym wypadku jesli znaleziono gdzies bardziej w srodku to tylko na 99%
+			{
+				Markers question(startingMarker, i, TextType::question, 99);
+				markersVector.push_back(question);
+			}
+			else               //jesli nie znaleziono 
+			{
+				//Sprawdzam czy zdanie ma jakakolwiek samogloske. Jesli jej nie ma to prawie na pewno jest to blad
+				
+				if (checkIfNoVowels(markersVector,text,startingMarker,i))
+				{
+					continue;
+				}
+				else
+				{
+					//Sprawdzam czy zdanie ma jakas spacje. Jesli nie ma, a jest bardzo dlugie to prawdopodobnie jest to zlepek roznych liter
 
-				//Wyszukiwanie w zdaniu jednego ze slow kluczowych zdefiniowanych w dictionary, charakteryzujacych pytanie
-				while (k < dictionary.size())
-				{
-					auto tmp= text.find(dictionary[k], startingMarker);
-					if (found != std::string::npos && tmp < found && tmp >= startingMarker)
+					if (checkIfNoSpaces(markersVector, text, startingMarker, i))
 					{
-						found = tmp;
-					}
-					else if (found == std::string::npos)
-					{
-						found = text.find(dictionary[k], startingMarker);
-					}
-					k++;
-				}
-				//Jezeli znaleziono takie slowo kluczowe na poczatku zdania to na 100% jest to pytanie
-				if (found - startingMarker < 4 && found!=std::string::npos && found < i)
-				{
-					Markers question(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::question, 100);
-					markersVector.push_back(question);
-				}
-				else if (found - startingMarker > 4 && found < i)  //w przeciwnym wypadku jesli znaleziono gdzies bardziej w srodku to tylko na 99%
-				{
-					Markers question(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::question, 99);
-					markersVector.push_back(question);
-				}
-				else               //jesli nie znaleziono 
-				{
-
-					//Sprawdzam czy zdanie ma jakakolwiek samogloske. Jesli jej nie ma to prawie na pewno jest to blad
-					auto found2 = text.find_first_of("eyuioa", startingMarker);
-					if (found2 == std::string::npos || found2 > i)
-					{
-						Markers unclasified(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::unclasified, 95);
-						markersVector.push_back(unclasified);
+						continue;
 					}
 					else
 					{
-						//Sprawdzam czy zdanie ma jakas spacje. Jesli nie ma, a jest bardzo dlugie to prawdopodobnie jest to zlepek roznych liter
-						auto found3 = text.find_first_of(" ", startingMarker+2);
-						if (i - startingMarker > 15 && (found3 > i || found3 == std::string::npos))
-						{
-							Markers unclasified(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::unclasified, 70);
-							markersVector.push_back(unclasified);
-						}
-						else
-						{
-							Markers question(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::question, 95);
-							markersVector.push_back(question);
-						}
+						Markers question(startingMarker, i, TextType::question, 95);
+						markersVector.push_back(question);
 					}
 				}
 			}
 		}
-		else if (text[i] == '!' || text[i] == '.' || ((i==text.size()-1) && (!markersVector.empty())))
+		else if (text[i] == '!' || text[i] == '.' || ((i == text.size() - 1) && (!markersVector.empty())))
 		{
 			//Wykrywanie przypadku gdy zdanie z niewiadomych przyczyn zaczyna sie znakiem interpunkcyjnym
 			if (i == 0)
@@ -123,51 +179,37 @@ std::vector<Markers> findQA(const std::string& text)
 				continue;
 			}
 
-			//"Cofanie siÄ™" dopoki nie znajdzie sie znaku interpunkcyjnego lub poczatku
-			int startingMarker = i - 1;
-			while (startingMarker != 0)
-			{
-				if (text[startingMarker] == '.' || text[startingMarker] == '!' || text[startingMarker] == '?')
-					break;
-				startingMarker--;
-			}
+			//"Cofanie siê" dopoki nie znajdzie sie znaku interpunkcyjnego lub poczatku
+
+			size_t startingMarker = findPreviousPunctuationMark(text, i);
+
 			//Wykrywanie sytuacji " ? . " i podobnych
-			if (i - startingMarker + 1 < 2)
+
+			if (checkIfEmptySentence(markersVector, text, startingMarker, i))
+				continue;
+
+			//Sprawdzam czy zdanie ma jakakolwiek samogloske. Jesli jej nie ma to prawie na pewno jest to blad
+			
+			if (checkIfNoVowels(markersVector, text, startingMarker, i))
 			{
-				Markers unclasified(startingMarker, i, TextType::unclasified, 60);
-				markersVector.push_back(unclasified);
 				continue;
 			}
 			else
 			{
-
-				//Sprawdzam czy zdanie ma jakakolwiek samogloske. Jesli jej nie ma to prawie na pewno jest to blad
-				auto found2 = text.find_first_of("eyuioa", startingMarker);
-				if (found2 == std::string::npos || found2 > i)
+				//Sprawdzam czy zdanie ma spacje. Jesli jej nie ma i jest bardzo dlugie to prawdopodobnie jest to zlepek liter
+				if (checkIfNoSpaces(markersVector, text, startingMarker, i))
 				{
-					Markers unclasified(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::unclasified, 95);
-					markersVector.push_back(unclasified);
+					continue;
 				}
 				else
 				{
-
-					//Sprawdzam czy zdanie ma spacje. Jesli jej nie ma i jest bardzo dlugie to prawdopodobnie jest to zlepek liter
-					auto found3 = text.find_first_of(" ", startingMarker+2);
-					if (i - startingMarker > 15 && (found3 > i || found3 == std::string::npos))
-					{
-						Markers unclasified(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::unclasified, 70);
-						markersVector.push_back(unclasified);
-					}
-					else
-					{
-						Markers answer(startingMarker == 0 ? 0 : startingMarker + 1, i, TextType::answer, 95);
-						markersVector.push_back(answer);
-					}
+					Markers answer(startingMarker, i, TextType::answer, 95);
+					markersVector.push_back(answer);
 				}
 			}
 		}
 	}
-	/*W przypadku gdy tekst jest bez zadnej interpunkcji wykonuje siÄ™ ta bardziej "zaawansowana" czesc analizy.
+	/*W przypadku gdy tekst jest bez zadnej interpunkcji wykonuje siê ta bardziej "zaawansowana" czesc analizy.
 	Jednakze analizowany tekst jest traktowany jako jedno zdanie, a nie wieksza ilosc zdan gdyz niemozliwe jest okreslenie
 	kiedy potencjalnie zaczyna sie nowe zdanie, gdy nie mamy interpunkcji*/
 	if (markersVector.empty())
@@ -190,20 +232,16 @@ std::vector<Markers> findQA(const std::string& text)
 		else
 		{
 			//Sprawdzam czy sa samogloski. Jesli nie ma to prawdopodobnie bledne zdanie
-			int found2 = text.find_first_of("eyuioa");
-			if (found2 == std::string::npos)
+			if (checkIfNoVowels(markersVector, text, 0, text.size() - 1))
 			{
-				Markers unclasified(0, text.size() - 1, TextType::unclasified, 99);
-				markersVector.push_back(unclasified);
+				return markersVector;
 			}
 			else
 			{
 				//Szukam spacji. Jesli nie ma i zdanie jest dlugie to prawdopodobnie zlepek liter
-				int found3 = text.find_first_of(" ");
-				if (found3 == std::string::npos && text.size()>15)
+				if (checkIfNoSpaces(markersVector, text, 0, text.size()))
 				{
-					Markers unclasified(0, text.size() - 1, TextType::unclasified, 70);
-					markersVector.push_back(unclasified);
+					return markersVector;
 				}
 				else
 				{
@@ -216,11 +254,11 @@ std::vector<Markers> findQA(const std::string& text)
 	return markersVector;
 }
 
-int Markers::getStart() const
+size_t Markers::getStart() const
 {
 	return start;
 }
-int Markers::getEnd() const
+size_t Markers::getEnd() const
 {
 	return end;
 }
@@ -232,4 +270,3 @@ int Markers::getPercentageChance() const
 {
 	return percentage_chance;
 }
-
--- source/textanalysis/text_analysis.h
+++ source/textanalysis/text_analysis.h
@@ -2,8 +2,7 @@
 #define TEXT_ANALYSIS
 #include<string>
 #include<vector>
-//Slowniczek slow kluczowych pomocnych przy szukaniu pytan
-extern const std::vector<std::string> dictionary;
+
 /* Typ enumeracyjny uzywany przy markerach*/
 enum TextType
 {
@@ -14,21 +13,21 @@ enum TextType
 /* Klasa ta przechowuje indeks poczatku (int start), indeks konca (int end) oraz wyzej wspomniany typ enum okreslajacy czy to jest question czy answer*/
 class Markers
 {
-	int start;
-	int end;
+	size_t start;
+	size_t end;
 	TextType type;
 	int percentage_chance;
 public:
-	Markers(int s, int e, TextType t, int percent) 
-	    : start(s)
-	    , end(e)
-	    , type(t)
-	    , percentage_chance(percent) 
+	Markers(size_t s, size_t e, TextType t, int percent) 
+		:start(s)
+		, end(e)
+		, type(t)
+		, percentage_chance(percent) 
 	{};
-	int getStart() const;
-	int getEnd() const;
+	size_t getStart() const;
+	size_t getEnd() const;
 	TextType getType() const;
 	int getPercentageChance() const;
 };
-std::vector<Markers> findQA(const std::string& text);
+std::vector<Markers> findQA(std::string text);
 #endif
