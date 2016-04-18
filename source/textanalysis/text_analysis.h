23500c00c99f864267063eb173fa8aa1166f8fc7
dwegrowski <dwegrowski@wp.pl>
2016-04-18 22:37:48 +0200

    Changed encoding to UTF8 without BOM and untabify

--- source/textanalysis/text_analysis.h
+++ source/textanalysis/text_analysis.h
@@ -6,28 +6,28 @@
 /* Typ enumeracyjny uzywany przy markerach*/
 enum TextType
 {
-	unclasified,
-	question,
-	answer
+    unclasified,
+    question,
+    answer
 };
 /* Klasa ta przechowuje indeks poczatku (int start), indeks konca (int end) oraz wyzej wspomniany typ enum okreslajacy czy to jest question czy answer*/
 class Markers
 {
-	size_t start;
-	size_t end;
-	TextType type;
-	int percentage_chance;
+    size_t start;
+    size_t end;
+    TextType type;
+    int percentage_chance;
 public:
-	Markers(size_t s, size_t e, TextType t, int percent) 
-		:start(s)
-		, end(e)
-		, type(t)
-		, percentage_chance(percent) 
-	{};
-	size_t getStart() const;
-	size_t getEnd() const;
-	TextType getType() const;
-	int getPercentageChance() const;
+    Markers(size_t s, size_t e, TextType t, int percent) 
+        :start(s)
+        , end(e)
+        , type(t)
+        , percentage_chance(percent) 
+    {};
+    size_t getStart() const;
+    size_t getEnd() const;
+    TextType getType() const;
+    int getPercentageChance() const;
 };
 std::vector<Markers> findQA(std::string text);
 #endif
