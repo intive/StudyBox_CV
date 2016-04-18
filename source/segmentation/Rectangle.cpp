4d374ef5af2f024c5f768cdc3ff4ba2f0fc692ea
Rafał Andrusieczko <andrusieczko.rafal@gmail.com>
2016-04-18 23:33:25 +0200

    Changed tabs to spaces.

--- source/segmentation/Rectangle.cpp
+++ source/segmentation/Rectangle.cpp
@@ -1,7 +1,7 @@
 #include "Rectangle.hpp"
 
 Rectangle::Rectangle(const cv::RotatedRect& rotatedRect)
-	: RotatedRect(rotatedRect)
+    : RotatedRect(rotatedRect)
 {
 }
 
@@ -11,49 +11,49 @@ Rectangle::Rectangle(const cv::RotatedRect& rotatedRect)
 
 const cv::Point Rectangle::topLeft() const
 {
-	cv::Rect rect = boundingRect();
-	return cv::Point(rect.x, rect.y);
+    cv::Rect rect = boundingRect();
+    return cv::Point(rect.x, rect.y);
 }
 
 const cv::Point Rectangle::topRight() const
 {
-	cv::Rect rect = boundingRect();
-	return cv::Point(rect.x + rect.width, rect.y);
+    cv::Rect rect = boundingRect();
+    return cv::Point(rect.x + rect.width, rect.y);
 }
 
 const cv::Point Rectangle::bottomLeft() const
 {
-	cv::Rect rect = boundingRect();
-	return cv::Point(rect.x, rect.y + rect.height);
+    cv::Rect rect = boundingRect();
+    return cv::Point(rect.x, rect.y + rect.height);
 }
 
 const cv::Point Rectangle::bottomRight() const
 {
-	cv::Rect rect = boundingRect();
-	return cv::Point(rect.x + rect.width, rect.y + rect.height);
+    cv::Rect rect = boundingRect();
+    return cv::Point(rect.x + rect.width, rect.y + rect.height);
 }
 
 Rectangle::operator Json() const
 {
-	Json j;
-	cv::Point2f topleft = topLeft();
-	j[top_left_x_Key] = topleft.x;
-	j[top_left_y_Key] = topleft.y;
+    Json j;
+    cv::Point2f topleft = topLeft();
+    j[top_left_x_Key] = topleft.x;
+    j[top_left_y_Key] = topleft.y;
 
-	cv::Point2f bottomRight = topRight();
-	j[bottom_right_x_Key] = bottomRight.x;
-	j[bottom_right_y_Key] = bottomRight.y;
+    cv::Point2f bottomRight = topRight();
+    j[bottom_right_x_Key] = bottomRight.x;
+    j[bottom_right_y_Key] = bottomRight.y;
 
-	return j;
+    return j;
 }
 
 Rectangle& Rectangle::operator * (const size_t& value)
 {
-	center.x *= value;
-	center.y *= value;
+    center.x *= value;
+    center.y *= value;
 
-	size.width *= value;
-	size.height *= value;
+    size.width *= value;
+    size.height *= value;
 
-	return *this;
+    return *this;
 }
--- source/segmentation/Rectangle.hpp
+++ source/segmentation/Rectangle.hpp
@@ -10,25 +10,25 @@
 class Rectangle : public cv::RotatedRect
 {
 public:
-	Rectangle(const cv::RotatedRect& rotatedRect);
-	//Rectangle(const Json& json);
-	~Rectangle() = default;
+    Rectangle(const cv::RotatedRect& rotatedRect);
+    //Rectangle(const Json& json);
+    ~Rectangle() = default;
 
-	const cv::Point topLeft() const;
-	const cv::Point topRight() const;
-	const cv::Point bottomLeft() const;
-	const cv::Point bottomRight() const;
+    const cv::Point topLeft() const;
+    const cv::Point topRight() const;
+    const cv::Point bottomLeft() const;
+    const cv::Point bottomRight() const;
 
-	operator Json() const;
-	Rectangle(const Rectangle& rectangle) = default;
-	Rectangle& operator = (const Rectangle& rectangle) = default;
-	Rectangle& operator * (const size_t& value);
+    operator Json() const;
+    Rectangle(const Rectangle& rectangle) = default;
+    Rectangle& operator = (const Rectangle& rectangle) = default;
+    Rectangle& operator * (const size_t& value);
 
 private:
-	static constexpr const char* const top_left_x_Key = "top_left_x";
-	static constexpr const char* const top_left_y_Key = "top_left_y";
-	static constexpr const char* const bottom_right_x_Key = "bottom_right_x";
-	static constexpr const char* const bottom_right_y_Key = "bottom_right_y";
+    static constexpr const char* const top_left_x_Key = "top_left_x";
+    static constexpr const char* const top_left_y_Key = "top_left_y";
+    static constexpr const char* const bottom_right_x_Key = "bottom_right_x";
+    static constexpr const char* const bottom_right_y_Key = "bottom_right_y";
 };
 
 #endif // RECTANGLE_HPP
