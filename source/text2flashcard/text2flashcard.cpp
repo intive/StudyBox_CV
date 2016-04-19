32793afb1990fa6384d5617e52b4223c6953cc62
dwegrowski <dwegrowski@wp.pl>
2016-04-20 00:17:03 +0200

    UTF8 bez BOM + untabify

--- source/text2flashcard/text2flashcard.cpp
+++ source/text2flashcard/text2flashcard.cpp
@@ -10,25 +10,25 @@ std::string textToJson(const std::string& txt)
     const std::string percentage_chance = "percentage_chance";
 
     //uzywam funkcji analizy tekstu
-	std::vector<Markers> markers = findQA(txt);
+    std::vector<Markers> markers = findQA(txt);
 
-    //pakujê wyniki do obiektu JSON
-	Json array = Json::Array();
-	for (size_t i = 0; i < markers.size(); i++)
-	{
-		Json object = {
-			{ begin , markers[i].getStart() },
-			{ end , markers[i].getEnd() },
-			{ type , (int)markers[i].getType() },
-			{ percentage_chance , markers[i].getPercentageChance() }
-		};
-		array.push_back(object);
-	}
-	Json final = {
-		{ "result" , array }
-	};
+    //pakujÄ™ wyniki do obiektu JSON
+    Json array = Json::Array();
+    for (size_t i = 0; i < markers.size(); i++)
+    {
+        Json object = {
+            { begin , markers[i].getStart() },
+            { end , markers[i].getEnd() },
+            { type , (int)markers[i].getType() },
+            { percentage_chance , markers[i].getPercentageChance() }
+        };
+        array.push_back(object);
+    }
+    Json final = {
+        { "result" , array }
+    };
 
     //serializacja do stringa (stringu?)
-	std::string output=final.serialize();
+    std::string output=final.serialize();
     return output;
 }
