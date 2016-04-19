cd0e25247cd84111e40ae65fbb375e1794e55c1b
Michal Sokolowski <sokolowski.michal.piotr@gmail.com>
2016-04-20 01:11:35 +0200

    Resolved conflict with master

--- premake5.lua
+++ premake5.lua
@@ -127,23 +127,23 @@ workspace 'StudyBox_CV'
             local config =
               [[<?xml version="1.0" encoding="utf-8"?>
                 <configuration>
-                    <disabledPackageSources>
-                        <add key="Microsoft and .NET" value="true" />
-                    </disabledPackageSources>
-                    <packageRestore>
-                        <add key="enabled" value="True" />
-                        <add key="automatic" value="True" />
-                    </packageRestore>
-                    <bindingRedirects>
-                        <add key="skip" value="False" />
-                    </bindingRedirects>
-                    <packageSources>
-                        <add key="nuget.org" value="https://api.nuget.org/v3/index.json" protocolVersion="3" />
-                        <add key="studyboxcv" value="]]..path..[[" />
-                    </packageSources>
-                    <activePackageSource>
-                        <add key="nuget.org" value="https://api.nuget.org/v3/index.json" />
-                    </activePackageSource>
+                <disabledPackageSources>
+                    <add key="Microsoft and .NET" value="true" />
+                </disabledPackageSources>
+                <packageRestore>
+                    <add key="enabled" value="True" />
+                    <add key="automatic" value="True" />
+                </packageRestore>
+                <bindingRedirects>
+                    <add key="skip" value="False" />
+                </bindingRedirects>
+                <packageSources>
+                    <add key="nuget.org" value="https://api.nuget.org/v3/index.json" protocolVersion="3" />
+                    <add key="studyboxcv" value="]]..path..[[" />
+                </packageSources>
+                <activePackageSource>
+                    <add key="nuget.org" value="https://api.nuget.org/v3/index.json" />
+                </activePackageSource>
                 </configuration>]]
                 file:write(config)
                 file:close()
@@ -178,14 +178,19 @@ workspace 'StudyBox_CV'
                 }
 
             if os.execute('sudo stat /usr/local/lib/libopencv_core.so.3.1.0 > /dev/null 2>&1') ~= 0 then
-                print('Setting up "OpenCV 3.1.0"')
+                print('Setting up "OpenCV"')
                 local tmp = os.tmpname()
                 local file = assert(io.open(tmp, 'w'))
                 file:write([[
-                    #!/bin/bash ; set -e
+                    #!/bin/bash
+                    set -e
+                    apt-get --yes update
                     apt-get --yes --force-yes install build-essential cmake git libgtk2.0-dev pkg-config libavcodec-dev libavformat-dev libswscale-dev python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev
-                    cd /opt ; git clone https://github.com/Itseez/opencv.git
-                    cd opencv ; git checkout 3.1.0
+                    cd /opt ; wget -O opencv.tar.gz https://github.com/Itseez/opencv/archive/3.1.0.tar.gz
+                    mkdir opencv ; tar -zxvf opencv.tar.gz -C opencv --strip-components 1
+                    cd opencv
+                    mkdir -p 3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e
+                    wget -P 3rdparty/ippicv/downloads/linux-808b791a6eac9ed78d32a7666804320e/ https://raw.githubusercontent.com/Itseez/opencv_3rdparty/81a676001ca8075ada498583e4166079e5744668/ippicv/ippicv_linux_20151201.tgz
                     for config in debug release
                     do
                         mkdir $config ; cd $config
@@ -193,39 +198,45 @@ workspace 'StudyBox_CV'
                         make ; make install ; cd ..
                     done
                     ldconfig
-                    cd / ; rm -rf /opt/opencv "$0"
                 ]])
                 file:close()
                 os.execute('sudo chmod +x '..tmp..'; sudo '..tmp)
+                os.execute('sudo rm -rf /opt/opencv /opt/opencv.tar.gz '..tmp)
             end
 
             if os.execute('sudo stat /usr/local/lib/libboost_unit_test_framework.so.1.60.0 > /dev/null 2>&1') ~= 0 then
-                print('Setting up "Boost Unit Test Framework 1.60"')
+                print('Setting up "Boost"')
                 local tmp = os.tmpname()
                 local file = assert(io.open(tmp, 'w'))
                 file:write([[
-                    #!/bin/bash ; set -e
-                    apt-get --yes --force-yes install p7zip-full
-                    cd /opt ; wget -O boost_1_60_0.7z "http://downloads.sourceforge.net/project/boost/boost/1.60.0/boost_1_60_0.7z?r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fboost%2Ffiles%2Fboost%2F1.60.0%2F&ts=1457189367&use_mirror=netcologne boost_1_60_0.7z"
-                    7z x boost_1_60_0.7z ; cd boost_1_60_0
+                    #!/bin/bash
+                    set -e
+                    apt-get --yes update
+                    apt-get --yes --force-yes install build-essential cmake git p7zip-full
+                    cd /opt ; wget -O boost.tar.gz https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.tar.gz
+                    mkdir boost ; tar -zxvf boost.tar.gz -C boost --strip-components 1
+                    cd boost
                     ./bootstrap.sh -with-libraries=test,log,random,thread,locale,regex,filesystem,date_time
                     ./b2 install
                     ldconfig
-                    cd / ; rm -rf /opt/boost_1_60_0 /opt/boost_1_60_0.7z "$0"
                 ]])
                 file:close()
                 os.execute('sudo chmod +x '..tmp..'; sudo '..tmp)
+                os.execute('sudo rm -rf /opt/boost /opt/boost.tar.gz '..tmp)
             end
 
-            if os.execute('sudo stat /usr/local/lib/libcpprest.so > /dev/null 2>&1') ~= 0 then
-                print('Setting up "Azure Storage CPP"')
+            if os.execute('sudo stat /usr/local/lib/libcpprest.so.2.7 > /dev/null 2>&1') ~= 0 then
+                print('Setting up "Rest"')
                 local tmp = os.tmpname()
                 local file = assert(io.open(tmp, 'w'))
                 file:write([[
-                    #!/bin/bash ; set -e
+                    #!/bin/bash
+                    set -e
+                    apt-get --yes update
                     apt-get --yes --force-yes install g++-4.8 g++ git make libssl-dev cmake libxml++2.6-dev libxml++2.6-doc uuid-dev
-                    cd /opt ; git clone https://github.com/Microsoft/cpprestsdk.git casablanca
-                    cd casablanca/Release
+                    cd /opt ; wget -O rest.tar.gz https://github.com/Microsoft/cpprestsdk/archive/v2.7.0.tar.gz
+                    mkdir rest ; tar -zxvf rest.tar.gz -C rest --strip-components 1
+                    cd rest/Release
                     for config in debug release
                     do
                         mkdir build_$config ; cd build_$config
@@ -233,22 +244,38 @@ workspace 'StudyBox_CV'
                         make ; make install ; cd ..
                     done
                     ldconfig
-                    cd /opt ; git clone https://github.com/Azure/azure-storage-cpp.git
-                    cd azure-storage-cpp/Microsoft.WindowsAzure.Storage
+                ]])
+                file:close()
+                os.execute('sudo chmod +x '..tmp..'; sudo '..tmp)
+                os.execute('sudo rm -rf /opt/rest /opt/rest.tar.gz '..tmp)
+            end
+
+            if os.execute('sudo stat /usr/local/lib/libazurestorage.so.2.2 > /dev/null 2>&1') ~= 0 then
+                print('Setting up "Azure"')
+                local tmp = os.tmpname()
+                local file = assert(io.open(tmp, 'w'))
+                file:write([[
+                    #!/bin/bash
+                    set -e
+                    apt-get --yes update
+                    apt-get --yes --force-yes install g++-4.8 g++ git make libssl-dev cmake libxml++2.6-dev libxml++2.6-doc uuid-dev
+                    cd /opt ; wget -O azure.tar.gz https://github.com/Azure/azure-storage-cpp/archive/v2.2.0.tar.gz
+                    mkdir azure ; tar -zxvf azure.tar.gz -C azure --strip-components 1
+                    cd azure/Microsoft.WindowsAzure.Storage
                     for config in debug release
                     do
                         mkdir build_$config ; cd build_$config
                         cmake -D CMAKE_BUILD_TYPE=$config -D CMAKE_INSTALL_PREFIX=/usr/local ..
-                        make ; make install
+                        make
                         mv -v Binaries/* /usr/local/lib/ ; cd ..
                     done
                     cp -r includes/was /usr/local/include/
                     cp -r includes/wascore /usr/local/include/
                     ldconfig
-                    cd / ; rm -rf /opt/casablanca /opt/azure-storage-cpp "$0"
                 ]])
                 file:close()
                 os.execute('sudo chmod +x '..tmp..'; sudo '..tmp)
+                os.execute('sudo rm -rf /opt/azure /opt/azure.tar.gz '..tmp)
             end
 
             if os.execute('sudo stat /usr/local/lib/libtesseract.so > /dev/null 2>&1') ~= 0 then
