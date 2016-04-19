4213369629a32da39b059a8fbf02a6afa1010ca0
dwegrowski <dwegrowski@wp.pl>
2016-04-20 00:07:52 +0200

    Serializacja do stringa + consty

--- source/text2flashcard/text2flashcard.cpp
+++ source/text2flashcard/text2flashcard.cpp
@@ -1,22 +1,34 @@
 #include "text2flashcard.h"
 #include <vector>
 
-void textToJson(const std::string& txt)
+std::string textToJson(const std::string& txt)
 {
+    
+    const std::string begin = "begin";
+    const std::string end = "end";
+    const std::string type = "type";
+    const std::string percentage_chance = "percentage_chance";
+
+    //uzywam funkcji analizy tekstu
 	std::vector<Markers> markers = findQA(txt);
+
+    //pakujê wyniki do obiektu JSON
 	Json array = Json::Array();
 	for (size_t i = 0; i < markers.size(); i++)
 	{
 		Json object = {
-			{ "begin" , markers[i].getStart() },
-			{ "end" , markers[i].getEnd() },
-			{ "type" , (int)markers[i].getType() },
-			{ "percentage_chance" , markers[i].getPercentageChance() }
+			{ begin , markers[i].getStart() },
+			{ end , markers[i].getEnd() },
+			{ type , (int)markers[i].getType() },
+			{ percentage_chance , markers[i].getPercentageChance() }
 		};
 		array.push_back(object);
 	}
 	Json final = {
 		{ "result" , array }
 	};
-	final.serialize("fiszka.txt");
+
+    //serializacja do stringa (stringu?)
+	std::string output=final.serialize();
+    return output;
 }
--- source/text2flashcard/text2flashcard.h
+++ source/text2flashcard/text2flashcard.h
@@ -3,6 +3,6 @@
 #include "../json/Json.hpp"
 #include "../textanalysis/text_analysis.h"
 
-void textToJson(const std::string& txt);
+std::string textToJson(const std::string& txt);
 
 #endif // TEXT2FLASHCARD
