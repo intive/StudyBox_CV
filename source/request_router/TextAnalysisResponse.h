a41304ab4fba2f78e674313130351183d66e6779
elizazych <eliza.zych@gmail.com>
2016-04-27 19:51:01 +0200

    Update TextAnalysisResponse.h

--- source/request_router/TextAnalysisResponse.h
+++ source/request_router/TextAnalysisResponse.h
@@ -8,6 +8,9 @@ namespace Router {
 	class RequestRouter;
 }
 
+//Tworzy odpowiedź na błędne zapytanie
+void CreateBadRequestError(Http::Response::Status& status, Json& response, const std::string& errorMessage);
+
 //Tworzy odpowiedź na zapytanie analizy tekstu
 std::pair<std::string, int> TextAnalysisResponse(const std::string& body);
 
