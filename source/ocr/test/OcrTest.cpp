188bf1a95adda8764ce2523fdf2757741e2c64ec
Michal Sokolowski <sokolowski.michal.piotr@gmail.com>
2016-04-30 04:45:05 +0200

    Added stream suppression

--- source/ocr/test/OcrTest.cpp
+++ source/ocr/test/OcrTest.cpp
@@ -1,7 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 
 #include "../Ocr.hpp"
+
+#if BOOST_OS_WINDOWS
+    #include <io.h>
+    #define dup(x) _dup((x))
+    #define dup2(x, y) _dup2((x), (y))
+    #define fileno(x) _fileno((x))
+    #define close(x) _close((x))
+    constexpr auto NULL_STREAM = "NUL";
+#else
+    #include <stdio.h>
+    #include <unistd.h>
+    constexpr auto NULL_STREAM = "/dev/null";
+#endif
 
 const std::string language = "pol+eng";
 const std::string datapath = std::string(ABSOLUTE_PATH) + "/res/tessdata";
@@ -11,25 +25,50 @@ const std::string imagepath = std::string(ABSOLUTE_PATH) + "/res/test/ocr_test.p
 const std::string imagetext = "This sentence is quite long\nand moderately complex.\n";
 const std::string regiontext = imagetext.substr(0, imagetext.find_first_of("\n") + 1);
 
+// Funkcja przekierowuje podany strumień do strumienia pustego,
+// wywołuje przekazaną funkcję i przywraca podany strumień do stanu początkowego
+void suppress(FILE* stream, std::function<void()> func)
+{
+    fflush(stream);
+    int fd = dup(fileno(stream));
+    if (!freopen(NULL_STREAM, "w", stream))
+    {
+        std::cerr << "Could not redirect stream." << std::endl;
+    }
+    func();
+    fflush(stream);
+    dup2(fd, fileno(stream));
+    close(fd);
+}
+
 /// Testy sprawdzające poprawność OCR
 BOOST_AUTO_TEST_SUITE(OcrTest)
 
 /// Sprawdza poprawność inicjalizacji OCR dla niepoprawnej ścieżki z danymi wyuczonymi
 BOOST_AUTO_TEST_CASE(SetupFailureData)
 {
-    BOOST_CHECK_THROW(Ocr("invalid_datapath"), std::runtime_error);
+    suppress(stderr, [](void)
+    {
+        BOOST_CHECK_THROW(Ocr("invalid_datapath"), std::runtime_error);
+    });
 }
 
 /// Sprawdza poprawność inicjalizacji OCR dla niepoprawnego języka
 BOOST_AUTO_TEST_CASE(SetupFailureLanguage)
 {
-    BOOST_CHECK_THROW(Ocr(datapath, "invalid_language"), std::runtime_error);
+    suppress(stderr, [](void)
+    {
+        BOOST_CHECK_THROW(Ocr(datapath, "invalid_language"), std::runtime_error);
+    });
 }
 
 /// Sprawdza poprawność inicjalizacji OCR dla niepoprawnej ścieżki z słownikiem
 BOOST_AUTO_TEST_CASE(SetupFailureDict)
 {
-    BOOST_CHECK_THROW(Ocr(datapath, language, "invalid_dictpath"), std::runtime_error);
+    suppress(stderr, [](void)
+    {
+        BOOST_CHECK_THROW(Ocr(datapath, language, "invalid_dictpath"), std::runtime_error);
+    });
 }
 
 /// Sprawdza poprawność inicjalizacji OCR
