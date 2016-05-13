cc4960f093af627a197c3dd51d92b1329ece91de
gajdzik <przemek11@autograf.pl>
2016-05-13 20:31:13 +0200

    Spacing

--- flashcards_analysis/flashcards_analysis.h
+++ flashcards_analysis/flashcards_analysis.h
@@ -6,18 +6,18 @@
 
 class Flashcard
 {
-	std::string question;
-	std::string answer;
-	std::vector<std::string> tips;
+    std::string question;
+    std::string answer;
+    std::vector<std::string> tips;
 public:
     Flashcard(std::string q, std::string a, std::vector<std::string> t)
         :question(q)
-		, answer(a)
-		, tips(t)
-	{};
-	std::string getQuestion() const;
-	std::string getAnswer() const;
-	std::vector<std::string> getTips() const;
+        , answer(a)
+    , tips(t)
+    {};
+    std::string getQuestion() const;
+    std::string getAnswer() const;
+    std::vector<std::string> getTips() const;
 };
 
 std::vector<Flashcard> flashcardsFromImage(const cv::Mat img);
