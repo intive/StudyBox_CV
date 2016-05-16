50b089f651c8d7ac6c6d5c9ecdfc0a9014008e27
gajdzik <przemek11@autograf.pl>
2016-05-16 15:03:08 +0200

    usuniecie klasy

--- source/flashcards_analysis/flashcards_analysis.h
+++ source/flashcards_analysis/flashcards_analysis.h
@@ -1,26 +1,8 @@
 #ifndef FLASHCARDS_ANALYSIS
 #define FLASHCARDS_ANALYSIS
-#include <string>
-#include <vector>
 #include "opencv2/opencv.hpp"
 #include "../json/Json.hpp"
 
-class Flashcard
-{
-    std::string question;
-    std::string answer;
-    std::vector<std::string> tips;
-public:
-    Flashcard(const std::string& q, const std::string& a, const std::vector<std::string>& t)
-        :question(q)
-        , answer(a)
-        , tips(t)
-    {};
-    const std::string& getQuestion() const;
-    const std::string& getAnswer() const;
-    const std::vector<std::string>& getTips() const;
-};
-
-const Json::Array& flashcardsToJson(const cv::Mat& img);
+Json::Array flashcardsToJson(const cv::Mat& img);
 
 #endif
